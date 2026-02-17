29417dd0f3ca119184fc63a1f8dc13f498443a91
nekabr.pkg <q123dbb@gmail.com>
2026-02-17 20:17:54 +0300

    Fix: Re-register LiquidGlassView.swift and resolve duplicate plugin registration in AppDelegate.swift

--- frontend/ios/Runner/AppDelegate.swift
+++ frontend/ios/Runner/AppDelegate.swift
@@ -8,9 +8,9 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     // РЕГИСТРАЦИЯ ФАБРИКИ СТЕКЛА
-    weak var registrar = self.registrar(forPlugin: "liquid-glass")
+    let registrar = self.registrar(forPlugin: "liquid-glass")
     let factory = LiquidGlassFactory(messenger: registrar!.messenger())
-    self.registrar(forPlugin: "liquid-glass")!.register(factory, withId: "liquid-glass-view")
+    registrar!.register(factory, withId: "liquid-glass-view")
     
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
